fdd04edd5552d8bf9d702197055dcaf998e3122c
Yuya Harada <tzou11291996@gmail.com>
2020-04-16 07:36:42 +0900

    first commit

--- populatron.js
+++ populatron.js
@@ -1,5 +1,19 @@
+const csv = require("csv-parser");
 const fs = require("fs");
+const results = [];
+
+fs.createReadStream("cities.csv")
+  .pipe(csv())
+  .on("data", (data) => results.push(data))
+  .on("end", () => {});
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    let total = 0;
+    for (let i = 0; i < results.length; i++) {
+      let num = Number(results[i].population);
+      total = total + num;
+    }
+    onFinished(total);
+  },
 };
